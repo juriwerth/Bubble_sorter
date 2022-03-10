0a8136571597c273243dc8b4551faa1ed328977d
Juri Werth <61845291+juriwerth@users.noreply.github.com>
2022-03-10 22:36:07 +0100

    Add files via upload

---
+++ array_bubble_sorter.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+list = np.random.randint(1,100,np.random.randint(10,30))
+print('List: '+str(list))
+
+for i in range(len(list)):
+    for j in range(len(list)-1):
+        if list[j] > list[j+1]:
+            temp = list[j]
+            list[j] = list[j+1]
+            list[j+1] = temp
+    print(str(i+1)+': ',list)
