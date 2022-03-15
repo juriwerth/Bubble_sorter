4304fc3163d8c5e37588ca663366450dac914cff
Juri Werth <juriwerth1@gmail.com>
2022-03-15 03:08:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,12 +12,13 @@ function draw() {
     newArray = false;
   }
   background(255);
-  for (j = 0; j < width; j++) {
-    stroke(255, 0, arr[j]);
-    line(j, windowHeight, j, arr[j]);
+  for (j = 0; j <= width; j++) {
+    stroke(255, 0, 255-arr[j]);
+    line(j, height, j, height-arr[j]);
   }
+  console.log(arr[0], arr[1]);
   for (l = 0; l < width; l++) {
-    if (arr[l] < arr[l+1]) {
+    if (arr[l] > arr[l+1]) {
       temp = arr[l];
       arr[l] = arr[l+1];
       arr[l+1] = temp;
